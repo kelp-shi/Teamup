541bd71ded4d4cc167984ffad3eb41f71ee36d0c
yuuya <blk1012y2@gmail.com>
2023-06-14 01:05:19 +0900

    プロジェクト削除：URL有効化

--- teamup_site/urls.py
+++ teamup_site/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('projects/List/', ProjectList.as_view(), name='projectlist'),
     path('projects/post/', ProjectCreateView.as_view(), name='postproject'),
     path('detail/<int:pk>/', views.ProjectDetail, name='detail'),
-    #path('<int:pk>/delete/', views.Pro, name='projectdelete'),
+    path('<int:pk>/delete/', views.Pro, name='projectdelete'),
     path('profile/<str:username>/', ProfileView.as_view(), name='profile'),
     path('help/', views.Help, name='help'),
 ]
